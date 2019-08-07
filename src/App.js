e2b4b2d59e09005c62299d8129e777f65fd2ae5e
Yuri Alves <yurialves2@gmail.com>
2019-08-07 13:49:13 -0300

    app styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
 
-function App() {
+import './App.css';
+import Login from './pages/login';
+
+
+export default function App() {
   return (
-    <div className="App">
-    </div>
+    <Login />
   );
 }
-
-export default App;
