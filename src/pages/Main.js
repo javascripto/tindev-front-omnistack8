720da23dd04c1b87e90488cf4ef92894f475937b
Yuri Alves <yurialves2@gmail.com>
2019-08-08 11:00:58 -0300

    styling main component

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,36 @@
 import React from 'react';
 
-export default function Main() {
-  return <h1>Hello</h1>;
+import './Main.css';
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
+
+export default function Main({ match: { params: { id }} }) {
+  console.log(id);
+  return (
+    <div className="main-container">
+      <img src={logo} alt="Tindev" />
+      <ul>
+        { [1, 2, 3, 4].map(_ => 
+        <li>
+          <img src="https://avatars0.githubusercontent.com/u/28929274?v=4" alt="" />
+          <footer>
+            <strong>Nome</strong>
+            <p>Descrição</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+        )}
+      </ul>
+    </div>
+  );
 }
